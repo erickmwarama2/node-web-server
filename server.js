411b75a2ae804d21ed518e18c3aec57db83e2780
erickmwarama <erickmwarama@gmail.com>
2021-09-29 20:40:33 +0300

    changes to server js to accomodate heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use((req, res, next)=>{
@@ -78,5 +80,5 @@ hbs.registerHelper('screamIt', (text)=>{
 });
 
 app.listen(3000, ()=>{
-  console.log("Server listening on port 3000")
+  console.log(`Server listening on port ${port}`);
 });
